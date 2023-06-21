fb3439243f0179f47d91eb9a621a1886e8676a9f
Kauan <kauankenney16752@gmail.com>
2023-06-20 23:27:07 -0300

    Primeira classe- Posicao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import boardgame.Posicao;
+
 public class Program {
     public static void main(String[] args) {
-
+        Posicao p = new Posicao(1,2);
+        System.out.println(p.toString());
     }
 }
